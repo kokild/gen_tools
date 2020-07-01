f51e29aec6533ba14346b3d0099937d23cb4c08b
Kokil K Deuri <kokdeuri@in.ibm.com>
2020-07-01 21:03:38 +0530

    run_in_conda.sh: If CONDA_PREFIX env variable is NULL, then get it using conda info

--- run_in_conda.sh
+++ run_in_conda.sh
@@ -9,20 +9,26 @@ function usage
 
 function main
 {
-	if ! which conda >/dev/null 2>&1
+	if ! type conda >/dev/null 2>&1
 	then
 		echo "Cannot find conda!"
 		return 1
 	fi
 
-	if [ `conda info | grep "active environment" | cut -d: -f2` != base ]
+    cur_active_env=`conda info | grep "active environment" | cut -d: -f2 | awk '{print $1}'`
+	if [ "$cur_active_env" != base -a "$cur_active_env" != None ]
 	then
-		echo "You must be in base env to start with"
+		echo "You must be in base env or None to start with"
 		echo "Please do 'conda deactivate'"
-		return 2
+		return 1
 	#else
 	#	echo "Good, you are in base"
 	fi
+
+    if [ -z "$CONDA_PREFIX" ]
+    then
+        CONDA_PREFIX=`conda info | grep "base environment :" | cut -d: -f2 | awk '{print $1}'`
+    fi
 
 	env_name=$1
 	shift
@@ -31,20 +37,20 @@ function main
 	if [ -z "$env_name" -o -z "$1" ]
 	then
 		usage
-		return 3
+		return 1
 	fi
 
 	if ! conda info --envs 2>/dev/null | grep -q -w $env_name
 	then
 		echo "There is no conda env named: $env_name"
 		usage
-		return 4
+		return 1
 	fi
 
 	cmd="source $CONDA_PREFIX/etc/profile.d/conda.sh && conda activate $CONDA_PREFIX/envs/$env_name > /dev/null 2>&1"
 	cmd=$cmd" && $@ ; conda deactivate"
 	#echo "Full cmd is:[$cmd]"
-	eval $cmd
+	eval "$cmd"
 }
 
 main "$@"
